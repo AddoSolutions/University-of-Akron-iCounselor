a51b8a51555a890763292385b4d1f09a1c683f24
Brian Kovacs <brian.kovacs@me.com>
2013-11-20 13:14:25 -0500

    Created the iCouncilFourthViewController class.

---
+++ iCounselor/iCounselor/iCouncilFourthViewController.h
@@ -0,0 +1,13 @@
+//
+//  iCouncilFourthViewController.h
+//  iCounselor
+//
+//  Created by Brian Kovacs on 11/20/13.
+//  Copyright (c) 2013 Addo Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface iCouncilFourthViewController : UINavigationController
+
+@end
---
+++ iCounselor/iCounselor/iCouncilFourthViewController.m
@@ -0,0 +1,38 @@
+//
+//  iCouncilFourthViewController.m
+//  iCounselor
+//
+//  Created by Brian Kovacs on 11/20/13.
+//  Copyright (c) 2013 Addo Solutions. All rights reserved.
+//
+
+#import "iCouncilFourthViewController.h"
+
+@interface iCouncilFourthViewController ()
+
+@end
+
+@implementation iCouncilFourthViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
