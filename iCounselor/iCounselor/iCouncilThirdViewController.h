c7350ba8fcf33a8f074ba52494a90974391f028f
Brian Kovacs <brian.kovacs@me.com>
2013-11-20 13:10:01 -0500

    Created the iCouncilThirdViewController clsss

---
+++ iCounselor/iCounselor/iCouncilThirdViewController.h
@@ -0,0 +1,13 @@
+//
+//  iCouncilThirdViewController.h
+//  iCounselor
+//
+//  Created by Brian Kovacs on 11/20/13.
+//  Copyright (c) 2013 Addo Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface iCouncilThirdViewController : UINavigationController
+
+@end
---
+++ iCounselor/iCounselor/iCouncilThirdViewController.m
@@ -0,0 +1,38 @@
+//
+//  iCouncilThirdViewController.m
+//  iCounselor
+//
+//  Created by Brian Kovacs on 11/20/13.
+//  Copyright (c) 2013 Addo Solutions. All rights reserved.
+//
+
+#import "iCouncilThirdViewController.h"
+
+@interface iCouncilThirdViewController ()
+
+@end
+
+@implementation iCouncilThirdViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
