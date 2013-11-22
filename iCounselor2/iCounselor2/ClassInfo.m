9a4f6ee9ded421928573cfcf6ccae6f74d12fad9
Joe Fortunato <jmf94@zips.uakron.edu>
2013-11-22 15:01:27 -0500

    Added Entities to Core Data schema, added necessary NSManagedObject classes for each Entity, and app can run without crashing!

---
+++ iCounselor2/iCounselor2/ClassInfo.m
@@ -0,0 +1,25 @@
+//
+//  ClassInfo.m
+//  iCounselor2
+//
+//  Created by Fortunato,Joseph on 11/22/13.
+//  Copyright (c) 2013 Akron. All rights reserved.
+//
+
+#import "ClassInfo.h"
+#import "ClassInfo.h"
+
+
+@implementation ClassInfo
+
+@dynamic subject;
+@dynamic number;
+@dynamic name;
+@dynamic details;
+@dynamic credits;
+@dynamic available;
+@dynamic alternatives;
+@dynamic prerequisites;
+@dynamic tags;
+
+@end
