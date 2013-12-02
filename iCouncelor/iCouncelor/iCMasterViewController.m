f3b47fda8300e673574999911a15c55eb91498e2
Nick Artman <nick@addosolutions.com>
2013-12-02 17:34:13 -0500

    Removed the unnecessary add button.

--- iCouncelor/iCouncelor/iCMasterViewController.m
+++ iCouncelor/iCouncelor/iCMasterViewController.m
@@ -31,8 +31,6 @@
 	// Do any additional setup after loading the view, typically from a nib.
     self.navigationItem.leftBarButtonItem = self.editButtonItem;
 
-    UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
-    self.navigationItem.rightBarButtonItem = addButton;
     self.detailViewController = (iCDetailViewController *)[[self.splitViewController.viewControllers lastObject] topViewController];
 }
 
