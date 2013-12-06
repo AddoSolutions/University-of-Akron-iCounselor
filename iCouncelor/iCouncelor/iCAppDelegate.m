86a402d410fe2682fc7041e507169c96fa2ef92d
Brian Kovacs <brian.kovacs@me.com>
2013-12-06 14:19:25 -0500

    Revert dd38ab0..95b7124
    This rolls back to commit dd38ab07f96a616d5ac57996ba835cb8764d1623.

--- iCouncelor/iCouncelor/iCAppDelegate.m
+++ iCouncelor/iCouncelor/iCAppDelegate.m
@@ -46,9 +46,6 @@
         }
     }
     
-    [[iCloud sharedCloud] setDelegate:self]; // Set this if you plan to use the delegate
-    [[iCloud sharedCloud] setVerboseLogging:YES]; // We want detailed feedback about what's going on with iCloud, this is OFF by default
-    [[iCloud sharedCloud] updateFiles]; // Force iCloud Update: This is done automatically when changes are made, but we want to make sure the view is always updated when presented
     
     
     return YES;
@@ -102,36 +99,12 @@
         NSArray * allClasses = [self.managedObjectContext executeFetchRequest:allCars error:&error];
         //error handling goes here
         for (UAClass * class in allClasses) {
-            
-            /*
-             
-             Check if the class is NOT in the pList and has NOT been taken
-             
-             found = false;
-             
-             for (plist data){
-                if matches, found=true
-             }
-             
-             if (!found) deleteObject;
-             
-             */
-            
             [self.managedObjectContext deleteObject:class];
         }
         
         /* Re add the new ones */
         
         for (NSDictionary *eachClassRaw in classData) {
-            
-            
-            /*
-             
-             Check id class exists in DB, if so, load it and edit it, otherwise make one.
-             
-             
-             
-             */
             
             UAClass *class =[NSEntityDescription insertNewObjectForEntityForName:@"UAClass" inManagedObjectContext:self.managedObjectContext ];
             
